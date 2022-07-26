77c5735efbb7f119279fad726cab01a3e0a0455e
ArshesSH <105101004ksh@gmail.com>
2022-07-26 15:53:14 +0900

    Update

--- InhaWinAPIGame/Ray.h
+++ InhaWinAPIGame/Ray.h
@@ -12,28 +12,19 @@ public:
 		pos(pos)
 	{}
 
-	//void ToPoint( const Vec2<T>& targetPos )
-	//{
-	//	dir = (targetPos - pos).GetNormalized();
-	//}
-
 	bool CastToLine( const Vec2<T>& lineStart, const Vec2<T>& lineEnd ) const
 	{
 		bool a = (pos.y < lineStart.y);
 		bool b = (pos.y < lineEnd.y);
 		bool c = (pos.x < (lineEnd.x - lineStart.x)* (pos.y - lineStart.y) / (lineEnd.y - lineStart.y) + lineStart.x);
-
+		 
 		return (a != b) && c;
-
-		/*return ((pos.y < lineStart.y) != (pos.y < lineEnd.y)&&
-			pos.x < (lineEnd.x - lineStart.x)* (pos.y - lineStart.y) / (lineEnd.y - lineStart.y) + lineStart.x);*/
 	}
 
 	Vec2<T> GetCastedPos( const Vec2<T>& lineStart, const Vec2<T>& lineEnd ) const
 	{
 		return { 0,0 };
 	}
-
 
 private:
 	Vec2<T> pos;
